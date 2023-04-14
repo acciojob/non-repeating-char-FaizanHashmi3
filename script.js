0e6428410e52bd5ff57b5976261d78a7152a9f23
FaizanHashmi3 <96041026+FaizanHashmi3@users.noreply.github.com>
2023-04-14 16:38:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+var inp = prompt("Enter a string");
+
+function util(str)
+	{
+		let arr=[];
+		for(let i=0;i<str.length;i++)
+			{
+				arr[i]=str.charAt(i);
+			}
+		arr.sort();
+		// let ans=arr[0];
+		if(arr.length==1) return arr[0];
+		if((arr.length>=2)&&(arr[0]!=arr[1])) return arr[0];
+		for(let i=1;i<arr.length-1;i++)
+			{
+				if((arr[i]!=arr[i+1])&&(arr[i]!=arr[i-1]))
+				{
+					return arr[i];
+				}
+			}
+		if(arr[arr.length-1]!=arr[arr.length-2])
+		{
+			return arr[arr.length-1];
+		}
+		
+	}
+let res=util(inp);
+alert(res);
+
